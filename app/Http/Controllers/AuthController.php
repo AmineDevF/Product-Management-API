0ce2b84e7cfa12c3a7f71e8ba0d95d0a179815e4
Amine Formateur <amine.devinfo@gmail.com>
2023-09-18 16:07:04 +0200

    add login fonctionality + token

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,41 +4,55 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
 
-public function register(Request $request)
-{
-  
-
-   
-
-    // dd($request);
-
-$validatedData = $request->validate([
-                   'name' => 'required',
-                   'email' => 'required',
-                   'password' => 'required',
-                   'phone' => 'required',
-]);
+       public function register(Request $request)
+       {
 
 
-      $user = User::create([
-                   'name' => $validatedData['name'],
-                   'email' => $validatedData['email'],
-                   'password' => Hash::make($validatedData['password']),
-                   'phone' => $validatedData['phone'],
-                   
-       ]);
+              $validatedData = $request->validate([
+                     'name' => 'required',
+                     'email' => 'required',
+                     'password' => 'required',
+                     'phone' => 'required',
+              ]);
 
-$token = $user->createToken('auth_token')->plainTextToken;
 
-return response()->json([
-                    'access_token' => $token,
-                    'token_type' => 'Bearer',
-]);
-}
+              $user = User::create([
+                     'name' => $validatedData['name'],
+                     'email' => $validatedData['email'],
+                     'password' => Hash::make($validatedData['password']),
+                     'phone' => $validatedData['phone'],
+
+              ]);
+
+              $token = $user->createToken('auth_token')->plainTextToken;
+
+              return response()->json([
+                     'access_token' => $token,
+                     'token_type' => 'Bearer',
+              ]);
+       }
+       public function login(Request $request)
+       {
+              if (!Auth::attempt($request->only('email', 'password'))) {
+                     return response()->json([
+                            'message' => 'Invalid login details'
+                     ], 401);
+              }
+
+              $user = User::where('email', $request['email'])->firstOrFail();
+
+              $token = $user->createToken('auth_token')->plainTextToken;
+
+              return response()->json([
+                     'access_token' => $token,
+                     'token_type' => 'Bearer',
+              ]);
+       }
 }
